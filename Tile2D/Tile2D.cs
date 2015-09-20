b3f19975c588eea6524b5e99278b0b4e03f512cc
John Winstead <john@jwin.io>
2015-09-20 00:38:17 -0700

    begining of Tile2D

---
+++ Tile2D/Tile2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile2D : MonoBehaviour {
+  public List<Sprite> spriteList;
+  public List<string> tagList;
+
+  public string tyleType="none";
+  public bool collide=true;
+  public float transparency=0;
+
+	// Use this for initialization
+	void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
