668e2bcd526c14647169af19ec37ce0b405a042d
John Winstead <john@jwin.io>
2015-09-20 00:43:54 -0700

    update WEB to be WebService

---
+++ Shared/Scripts/WebService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+ 
+public class WebService {
+  public static string GetString(string url){
+    return (new WWW(url)).text;
+  }
+  public static JSONObject GetJSON(string url){
+    return (new WWW(url)).json();
+  }
+  public static string PostString(string url, string str){
+    Dictionary<string, string> headers = new Dictionary<string, string>();
+    headers.Add( "Content-Type", "application/json" );
+    return ( new WWW(url, str.ToBytes(), headers) ).text;
+  }
+  public static string PostStringGetJSON(string url, string str){
+    return JSONObject.new(PostString(url,str));
+  }
+  public static string PostJSON(string url, JSONObject json){
+    return PostString(url, json.Print());
+  }
+  public static JSONObject PostJSONGetJSON(string url, JSONObject json){
+    return JSONObject.new(PostJSON(url,json));
+  }
+}
